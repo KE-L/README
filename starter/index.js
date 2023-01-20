2675c5be210782248781beff69ecf74bc6c52303
kassy.essiet <jillanderson@air-21704.home>
2023-01-20 18:46:38 +0000

    added questions

--- starter/index.js
+++ starter/index.js
@@ -5,6 +5,55 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
+{ 
+    type: "input", 
+    message: "What is the title of your project?", 
+    name: "title", 
+}, 
+{ 
+    type: "input", 
+    message: "What is the your project about? Insert a description", 
+    name: "description", 
+}, 
+{ 
+    type: "input", 
+    message: "Insert a Table of Content", 
+    name: "Table of Content", 
+}, 
+{ 
+    type: "input", 
+    message: "How do you install your project? Insert an installation guide", 
+    name: "Installation", 
+}, 
+
+{ 
+    type: "input", 
+    message: "How do you use your project? Insert a usage guide", 
+    name: "Usage", 
+}, 
+{ 
+    type: "input", 
+    message: "Insert details on any applicable licenses", 
+    name: "Usage", 
+}, 
+{ 
+    type: "input", 
+    message: "Who contributed to the project? Insert contributor details", 
+    name: "Contributors", 
+}, 
+
+{ 
+    type: "input", 
+    message: "Insert any details on testing", 
+    name: "Tests", 
+}, 
+
+{ 
+    type: "input", 
+    message: "Invite Questions", 
+    name: "TestQuestionss", 
+}, 
+
 
 ];
 
